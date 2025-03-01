01145517daff5264ebcf920c5f87092c937f139a
Chadrack Mbuyi Kalala <80404784+Chadrackmbuyi01@users.noreply.github.com>
2025-03-01 14:20:55 +0200

    The service layer has been created and the user registration method has been created

--- src/main/java/za/ac/innotech/authentication/repository/UserRepository.java
+++ src/main/java/za/ac/innotech/authentication/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package za.ac.innotech.authentication.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.innotech.authentication.model.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
